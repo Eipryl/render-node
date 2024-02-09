bc147374427c88037001745b097ff05de9478f09
Robinson Carpio <robinsoncarpio68@gmail.com>
2024-02-08 19:51:27 -0500

    change port

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(3000)
-console.log('Server on port', 3000)
+app.listen(process.env.PORT || 3000)
+console.log('Server on port');
