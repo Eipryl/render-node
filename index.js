42ebc8537320db2682ddb08a6309fb386032adce
Robinson Carpio <robinsoncarpio68@gmail.com>
2024-02-09 01:28:45 -0500

    add postgresql

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import {config} from "dotenv";
+import pg from 'pg';
+
+config()
 
 const app = express();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/date', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 app.listen(process.env.PORT || 3000)
